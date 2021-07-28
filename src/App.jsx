8db5998ad32ee0b3c121c01bf9375ea704327a0b
Edrson <ederson.ramirez87@gmail.com>
2021-07-27 22:24:26 -0600

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 /** @format */
 
 import React from "react";
+import shortid from "shortid";
 
 function App() {
+  const [tarea, setTarea] = React.useState("");
+  const [tareas, setTareas] = React.useState([]);
+  const agregarTarea = (e) => {
+    e.preventDefault();
+    console.log(tarea);
+    if (!tarea.trim()) {
+      console.log("elemento vacio");
+      return;
+    }
+    setTareas([...tareas, { id: shortid.generate(), nombreTarea: tarea }]);
+    setTarea("");
+  };
+
   return (
     <div className="container mt-5">
       <h1 className="text-center">CRUD SIMPLE</h1>
@@ -24,6 +38,18 @@ function App() {
         </div>
         <div className="col-4">
           <h4 className="text-center">Formulario</h4>
+          <form onSubmit={agregarTarea}>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Ingrese tarea"
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <button className="btn btn-dark btn-block" type="submit">
+              Agregar
+            </button>
+          </form>
         </div>
       </div>
     </div>
